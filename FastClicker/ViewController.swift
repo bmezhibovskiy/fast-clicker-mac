618f2ced67c330c89390efd1c60e515fcc4e2839
Boris Igorevich Mezhibovskiy <mez13526@gmail.com>
2022-07-02 10:47:19 -0500

    Fast clicker v1

---
+++ FastClicker/ViewController.swift
@@ -0,0 +1,72 @@
+//
+//  ViewController.swift
+//  FastClicker
+//
+//  Created by Boris Mezhibovskiy on 7/1/22.
+//
+
+import Cocoa
+import ApplicationServices
+import AppKit
+import Quartz
+
+
+class ViewController: NSViewController {
+    var running = false
+    var dEvent: CGEvent?
+    var uEvent: CGEvent?
+
+    var clickDelay: TimeInterval {
+        let cps = cpsTextField.doubleValue
+        return 1.0/cps
+
+    }
+    let downUpDelay = 0.005
+    let maxClicks = 100
+    var numClicks = 0
+
+    @IBOutlet weak var outputLabel: NSTextFieldCell!
+    @IBOutlet weak var cpsTextField: NSTextField!
+
+    var mouseLoc: CGPoint {
+        let loc = NSEvent.mouseLocation
+        let screenHeight = NSScreen.main!.frame.height
+        return CGPoint(x: loc.x, y: screenHeight - loc.y)
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        NSEvent.addGlobalMonitorForEvents(matching: .keyDown) { event in
+            if event.keyCode == 50 { //` to start/stop running
+                self.running = !self.running
+                self.start()
+            }
+        }
+        start()
+    }
+    func start() {
+        if self.running {
+            self.doClick()
+            numClicks += 1
+            outputLabel.title = "clicks: \(numClicks)"
+//            if numClicks > maxClicks {
+//                running = false
+//            }
+        }
+    }
+
+    func doClick() {
+        self.dEvent = CGEvent(mouseEventSource: nil, mouseType: .leftMouseDown, mouseCursorPosition: mouseLoc, mouseButton: .left)
+        self.uEvent = CGEvent(mouseEventSource: nil, mouseType: .leftMouseUp, mouseCursorPosition: mouseLoc, mouseButton: .left)
+
+        self.dEvent!.post(tap: .cghidEventTap)
+        DispatchQueue.main.asyncAfter(deadline: .now()+self.downUpDelay) {
+            self.uEvent!.post(tap: .cghidEventTap)
+            DispatchQueue.main.asyncAfter(deadline: .now()+self.clickDelay) {
+                self.start()
+            }
+        }
+    }
+}
+
